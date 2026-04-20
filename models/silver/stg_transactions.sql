1e0462cc63c2a02e709f325a1285c92156bcdb3c
Ibrahim Yunusa <Ibrahimgbenga21@gmail.com>
2026-04-20 01:22:33 +0000

    silver layer

---
+++ models/silver/stg_transactions.sql
@@ -0,0 +1,31 @@
+with stg_transactions as (
+    select * from {{ ref("bronze_transactions")}}
+)
+
+select
+    transaction_id,
+    customer_id,
+    store_id,
+    product_id,
+    product_name,
+    category,
+    payment_method,
+    cast(quantity as integer) as quantity,
+    cast(unit_price as number(10,2)) as unit_price,
+    cast(total_amount as number(10,2)) as total_amount,
+    -- standardized timestamp
+    coalesce(
+        try_to_timestamp(transaction_timestamp, 'YYYY-MM-DD HH24:MI:SS'),
+        try_to_timestamp(transaction_timestamp, 'DD/MM/YYYY HH24:MI')
+    ) as trn_timestamp,
+
+    -- derived date
+    cast(
+        coalesce(
+            try_to_timestamp(transaction_timestamp, 'YYYY-MM-DD HH24:MI:SS'),
+            try_to_timestamp(transaction_timestamp, 'DD/MM/YYYY HH24:MI')
+        ) as date
+    ) as trn_date
+
+from 
+    stg_transactions
